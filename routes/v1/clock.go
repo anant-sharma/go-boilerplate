702d5202265180cda2b055d0b71d295115955783
Anant Sharma <anantatnet@gmail.com>
2019-06-25 17:19:04 +0530

    Refactored Routing

--- routes/v1/clock.go
+++ routes/v1/clock.go
@@ -1,1 +1,13 @@
 package v1router
+
+import (
+	v1controller "github.com/anant-sharma/go-boilerplate/controller/v1"
+	"github.com/gin-gonic/gin"
+)
+
+// InitClockRouter - Function to initialize clock router
+func InitClockRouter(router *gin.RouterGroup) {
+	c1 := v1controller.NewController()
+
+	router.GET("", c1.GetTimeStamp)
+}
--- routes/v1/router.go
+++ routes/v1/router.go
@@ -1,22 +1,12 @@
 package v1router
 
 import (
-	v1controller "github.com/anant-sharma/go-boilerplate/controller/v1"
 	"github.com/gin-gonic/gin"
 )
 
-// Router Interface
-type Router struct {
-}
-
 // InitRouter Function
 func InitRouter(v1 *gin.RouterGroup) {
-	c1 := v1controller.NewController()
 
-	{
-		clock := v1.Group("/clock")
-		{
-			clock.GET("", c1.GetTimeStamp)
-		}
-	}
+	InitClockRouter(v1.Group("/clock"))
+
 }
