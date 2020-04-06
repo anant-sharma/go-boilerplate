0919452c0d353be7425ef8ae61abaa52a2cda1b3
Anant Sharma <anantatnet@gmail.com>
2020-04-06 17:36:01 +0530

    :gift: Added Auth Middleware

--- routes/router.go
+++ routes/router.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/anant-sharma/go-boilerplate/config"
 	"github.com/anant-sharma/go-boilerplate/controller/health"
+	"github.com/anant-sharma/go-boilerplate/routes/middleware"
 	v1Router "github.com/anant-sharma/go-boilerplate/routes/v1"
 	"github.com/gin-gonic/gin"
 )
@@ -18,16 +19,16 @@ func InitRouter() {
 
 	r := gin.Default()
 
-	v1 := r.Group("/api/v1")
-	v1Router.InitRouter(v1)
-
 	r.GET("/healthz", func(c *gin.Context) {
 		c.JSON(http.StatusOK, health.Check(c))
 	})
 
-	/*
-		Start Server
-	*/
+	r.Use(middleware.AuthenticationRequired())
+
+	v1 := r.Group("/api/v1")
+	v1Router.InitRouter(v1)
+
+	// Start Server
 	err := r.Run(":" + strconv.Itoa(config.PORT))
 	if err != nil {
 		log.Fatal(err)
