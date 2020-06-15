131f0c938d4ed74b887bded0b97b994f39ca4051
Anant Sharma <anantatnet@gmail.com>
2020-06-15 14:47:29 +0530

    :gift: Updated Log Level

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"log"
+
 	router "github.com/anant-sharma/go-boilerplate/routes"
 )
 
 func main() {
+
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
 
 	router.InitRouter()
 
